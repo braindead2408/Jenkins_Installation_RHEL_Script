1c67ce117131b1bae0a37bf3ad4e06a65a4f3e6e
Tanmay Nagepatil <brainsniper2408@gmail.com>
2023-10-09 22:28:17 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Pre-requisites for installing Jenkins
+sudo yum install wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
